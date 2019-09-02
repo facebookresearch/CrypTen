7b05260f0c09f5504b536cdd884173f993aa9043
Xing Zhou <xingz@fb.com>
2019-09-02 11:51:40 -0700

    Additional modification of comments in mpc_imagenet/__init__.py
    Summary: Additional modification of comments in mpc_imagenet/__init__.py suggested since this file willl be visible outside.
    
    Reviewed By: shubho
    
    Differential Revision: D17146890
    
    fbshipit-source-id: e9382c67f630f70f2096b81346675af075b086fb

--- examples/mpc_imagenet/__init__.py
+++ examples/mpc_imagenet/__init__.py
@@ -5,5 +5,5 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
-# This file is currently empty because examples/TARGETS requires
-# __init__.py to build every example.
+# This file is currently empty because buck based internal build system
+# requires __init__.py to build every example.
