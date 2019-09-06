ae7a271e32420fe00a2dece9adf42455b9363926
Vinicius Reis <vini@fb.com>
2019-09-06 06:45:32 -0700

    Move benchmark framework to a helper class
    Summary:
    In follow-up diffs I'll add a new base class for our tests (in addition to
    MultiProcessTestCase). Moving the benchmark functions to a helper class so we
    can reuse code in that case.
    
    Reviewed By: shubho
    
    Differential Revision: D17209378
    
    fbshipit-source-id: aafdf07154c948f2e2337742bd845f6592aad75c

--- test/multiprocess_test_case.py
+++ test/multiprocess_test_case.py
@@ -10,21 +10,14 @@ import multiprocessing
 import os
 import sys
 import tempfile
-import time
 import unittest
-from collections import defaultdict
 from functools import wraps
-from typing import NamedTuple
 
 import numpy as np
 import torch
 import torch.distributed as dist
 
-
-class BenchmarkRun(NamedTuple):
-    name: str
-    niters: int
-    time: float
+from .benchmark_helper import BenchmarkHelper
 
 
 def get_random_test_tensor(max_value=6,
@@ -74,7 +67,6 @@ def get_random_linear(in_channels, out_channels):
 
 class MultiProcessTestCase(unittest.TestCase):
     MAIN_PROCESS_RANK = -1
-    _benchmark_results = defaultdict(list)
 
     @property
     def world_size(self):
@@ -108,6 +100,9 @@ class MultiProcessTestCase(unittest.TestCase):
                 fn = getattr(cls, attr)
                 setattr(cls, attr, cls.join_or_run(fn))
 
+        cls.benchmark_helper = None
+
+
     def __init__(self, methodName):
         super().__init__(methodName)
 
@@ -119,11 +114,17 @@ class MultiProcessTestCase(unittest.TestCase):
         self.benchmark_iters = 100 if self.benchmarks_enabled else 1
         self.default_tolerance = 0.5
 
+        cls = self.__class__
+        cls.benchmark_helper = BenchmarkHelper(self.benchmarks_enabled,
+                                               self.benchmark_iters)
+        if hasattr(self, 'benchmark_queue'):
+            cls.benchmark_helper.queue = self.benchmark_queue
+
         # This gets called in the children process as well to give subclasses a
         # chance to initialize themselves in the new process
         if self.rank == self.MAIN_PROCESS_RANK:
             self.file = tempfile.NamedTemporaryFile(delete=True).name
-            self._benchmark_queue = multiprocessing.Queue()
+
 
             self.processes = [
                 self._spawn_process(rank) for rank in range(int(self.world_size))
@@ -131,13 +132,14 @@ class MultiProcessTestCase(unittest.TestCase):
 
     def tearDown(self):
         super(MultiProcessTestCase, self).tearDown()
-        self._drain_benchmark_queue()
+        self.__class__.benchmark_helper.drain_benchmark_queue()
         for p in self.processes:
             p.terminate()
 
     @classmethod
     def tearDownClass(cls):
-        cls._print_benchmark_summary()
+        if cls.benchmark_helper:
+            cls.benchmark_helper.print_benchmark_summary(cls.__name__)
 
     def _current_test_name(self):
         # self.id() == e.g. '__main__.TestDistributed.TestAdditive.test_get_rank'
@@ -149,7 +151,7 @@ class MultiProcessTestCase(unittest.TestCase):
         process = multiprocessing.Process(
             target=self.__class__._run,
             name=name,
-            args=(test_name, rank, self._benchmark_queue, self.file),
+            args=(test_name, rank, self.__class__.benchmark_helper.queue, self.file),
         )
         process.start()
         return process
@@ -160,7 +162,7 @@ class MultiProcessTestCase(unittest.TestCase):
 
         self.file = file
         self.rank = int(rank)
-        self._benchmark_queue = queue
+        self.benchmark_queue = queue
 
         # set environment variables:
         communicator_args = {
@@ -186,81 +188,5 @@ class MultiProcessTestCase(unittest.TestCase):
     def _check_return_codes(self, process):
         self.assertEqual(process.exitcode, 0)
 
-    def _add_benchmark_results(self, rank, args, time, niters):
-        args_str = ", ".join([f"{k}={v}" for k, v in args.items()])
-        benchmark_name = f"{self._current_test_name()} ({args_str})"
-        self._benchmark_queue.put(
-            BenchmarkRun(name=benchmark_name, niters=niters, time=time)
-        )
-
     def benchmark(self, niters=None, data=None, **kwargs):
-        class Benchmark:
-            def __init__(self, test_case, niters, data=None, **kwargs):
-                if data is not None:
-                    niters = len(data)
-                    self.data = data
-
-                self.niters = niters
-                self.iters = range(self.niters)
-                self.args = kwargs
-                self.test_case = test_case
-
-            def __enter__(self):
-                self.start_time = time.perf_counter()
-                return self
-
-            def __exit__(self, etype, evalue, etraceback):
-                self.end_time = time.perf_counter()
-                self.test_case._add_benchmark_results(
-                    self.test_case.rank,
-                    self.args,
-                    self.end_time - self.start_time,
-                    self.niters,
-                )
-
-        if niters is None:
-            niters = self.benchmark_iters
-        if not self.benchmarks_enabled:
-            niters = 1
-
-        return Benchmark(data=data, niters=niters, test_case=self, **kwargs)
-
-    def _drain_benchmark_queue(self):
-        while not self._benchmark_queue.empty():
-            run = self._benchmark_queue.get()
-            MultiProcessTestCase._benchmark_results[run.name].append(run)
-
-    @classmethod
-    def _print_benchmark_summary(cls):
-        if not cls.benchmarks_enabled:
-            return
-
-        def format_time(time: float) -> str:
-            units = ["s", "ms", "us", "ns"]
-            idx = 0
-            while time < 1 and idx < len(units):
-                time *= 1000
-                idx += 1
-            return f"{time:10.3f}{units[idx]}"
-
-        def log(message: str):
-            print(message, file=sys.stderr)
-
-        log(f"Benchmark summary for {cls.__name__}")
-        log("-" * 80)
-
-        name = "Benchmark"
-        time = "Time per iteration"
-        niters = "Iterations"
-        iters_sec = "Iterations per second"
-
-        log(f"{name:<100}{time:>20}{niters:>20}{iters_sec:>25}")
-        log("-" * 80)
-        for name, runs in MultiProcessTestCase._benchmark_results.items():
-            avg_time = np.mean([x.time for x in runs])
-            niters = runs[0].niters
-            time = format_time(avg_time / niters)
-            iters_sec = int(niters / avg_time)
-
-            log(f"{name:<100}{time:>20}{niters:>20}{iters_sec:>25}")
-        log("-" * 80)
+        return self.benchmark_helper.benchmark(self, niters, data, **kwargs)
