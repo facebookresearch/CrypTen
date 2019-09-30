4901d4f97db57d0c1b04193259d2bcf8e6fb4a95
Shobha Venkataraman <shobha@fb.com>
2019-09-29 18:01:40 -0700

    Changes required for tutorials to use load/save (#74)
    Summary:
    Pull Request resolved: https://github.com/fairinternal/CrypTen/pull/74
    
    Updated mnist_utils.py so that options required by tutorials are added
    
    Reviewed By: shubho
    
    Differential Revision: D17660028
    
    fbshipit-source-id: bb7b7fd969900e1a92bc750c9e8e74d068b8be84

--- examples/mnist_utils.py
+++ examples/mnist_utils.py
@@ -5,6 +5,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import argparse
 import os
 
 import torch
@@ -13,7 +14,7 @@ from torchvision import datasets, transforms
 
 def process_mnist_files(raw_dir, processed_dir):
     """
-    Uncompresse zipped train and/or test image and label files, load the
+    Uncompress zipped train and/or test image and label files, load the
     uncompressed data files, and save to .pt files so that datasets.MNIST
     can read it directly.
     """
@@ -78,8 +79,8 @@ def _get_norm_mnist(dir):
     return mnist_norm, mnist_labels
 
 
-def split_features(split=0.5, dir="/tmp"):
-    """Splits features between Alice and Bob"""
+def split_features(split=0.5, dir="/tmp", party1="alice", party2="bob"):
+    """Splits features between Party 1 and Party 2"""
     mnist_norm, mnist_labels = _get_norm_mnist(dir)
     mnist_train_norm, mnist_test_norm = mnist_norm
     mnist_train_labels, mnist_test_labels = mnist_labels
@@ -87,21 +88,21 @@ def split_features(split=0.5, dir="/tmp"):
     num_features = mnist_train_norm.shape[1]
     split_point = int(split * num_features)
 
-    alice_train = mnist_train_norm[:, :split_point, :]
-    bob_train = mnist_train_norm[:, split_point:, :]
-    alice_test = mnist_test_norm[:, :split_point, :]
-    bob_test = mnist_test_norm[:, split_point:, :]
+    party1_train = mnist_train_norm[:, :, :split_point]
+    party2_train = mnist_train_norm[:, :, split_point:]
+    party1_test = mnist_test_norm[:, :, :split_point]
+    party2_test = mnist_test_norm[:, :, split_point:]
 
-    torch.save(alice_train, os.path.join(dir, "alice_train.pth"))
-    torch.save(bob_train, os.path.join(dir, "bob_train.pth"))
-    torch.save(alice_test, os.path.join(dir, "alice_test.pt"))
-    torch.save(bob_test, os.path.join(dir, "bob_test.pth"))
+    torch.save(party1_train, os.path.join(dir, party1 + "_train.pth"))
+    torch.save(party2_train, os.path.join(dir, party2 + "_train.pth"))
+    torch.save(party1_test, os.path.join(dir, party1 + "_test.pth"))
+    torch.save(party2_test, os.path.join(dir, party2 + "_test.pth"))
     torch.save(mnist_train_labels, os.path.join(dir, "train_labels.pth"))
     torch.save(mnist_test_labels, os.path.join(dir, "test_labels.pth"))
 
 
-def split_observations(split=0.5, dir="/tmp"):
-    """Splits observations between Alice and Bob"""
+def split_observations(split=0.5, dir="/tmp", party1="alice", party2="bob"):
+    """Splits observations between Party 1 and Party 2"""
     mnist_norm, mnist_labels = _get_norm_mnist(dir)
     mnist_train_norm, mnist_test_norm = mnist_norm
     mnist_train_labels, mnist_test_labels = mnist_labels
@@ -111,34 +112,88 @@ def split_observations(split=0.5, dir="/tmp"):
     num_test_obs = mnist_test_norm.shape[0]
     obs_test_split = int(split * num_test_obs)
 
-    alice_train = mnist_train_norm[:obs_train_split, :, :]
-    bob_train = mnist_train_norm[obs_train_split:, :, :]
-    alice_test = mnist_test_norm[:obs_test_split, :, :]
-    bob_test = mnist_test_norm[obs_test_split:, :, :]
+    party1_train = mnist_train_norm[:obs_train_split, :, :]
+    party2_train = mnist_train_norm[obs_train_split:, :, :]
+    party1_test = mnist_test_norm[:obs_test_split, :, :]
+    party2_test = mnist_test_norm[obs_test_split:, :, :]
+    torch.save(party1_train, os.path.join(dir, party1 + "_train.pth"))
+    torch.save(party2_train, os.path.join(dir, party2 + "_train.pth"))
+    torch.save(party1_test, os.path.join(dir, party1 + "_test.pth"))
+    torch.save(party2_test, os.path.join(dir, party2 + "_test.pth"))
 
-    torch.save(alice_train, os.path.join(dir, "alice_train.pth"))
-    torch.save(bob_train, os.path.join(dir, "bob_train.pth"))
-    torch.save(alice_test, os.path.join(dir, "alice_test.pth"))
-    torch.save(bob_test, os.path.join(dir, "bob_test.pth"))
+    party1_train_labels = mnist_train_labels[:obs_train_split]
+    party1_test_labels = mnist_test_labels[:obs_test_split]
+    party2_train_labels = mnist_train_labels[obs_train_split:]
+    party2_test_labels = mnist_test_labels[obs_test_split:]
 
-    alice_train_labels = mnist_train_labels[:obs_train_split]
-    alice_test_labels = mnist_test_labels[:obs_test_split]
-    bob_train_labels = mnist_train_labels[obs_train_split:]
-    bob_test_labels = mnist_test_labels[obs_test_split:]
-
-    torch.save(alice_train_labels, os.path.join(dir, "alice_train_labels.pth"))
-    torch.save(alice_test_labels, os.path.join(dir, "alice_test_labels.pth"))
-    torch.save(bob_train_labels, os.path.join(dir, "bob_train_labels.pth"))
-    torch.save(bob_test_labels, os.path.join(dir, "bob_test_labels.pth"))
+    torch.save(party1_train_labels, os.path.join(dir, party1 + "_train_labels.pth"))
+    torch.save(party1_test_labels, os.path.join(dir, party1 + "_test_labels.pth"))
+    torch.save(party2_train_labels, os.path.join(dir, party2 + "_train_labels.pth"))
+    torch.save(party2_test_labels, os.path.join(dir, party2 + "_test_labels.pth"))
 
 
-def split_features_v_labels(dir="/tmp"):
-    """Gives Alice features and Bob labels"""
+def split_features_v_labels(dir="/tmp", party1="alice", party2="bob"):
+    """Gives Party 1 features and Party 2 labels"""
     mnist_norm, mnist_labels = _get_norm_mnist(dir)
     mnist_train_norm, mnist_test_norm = mnist_norm
     mnist_train_labels, mnist_test_labels = mnist_labels
 
-    torch.save(mnist_train_norm, os.path.join(dir, "alice_train.pth"))
-    torch.save(mnist_test_norm, os.path.join(dir, "alice_test.pth"))
-    torch.save(mnist_train_labels, os.path.join(dir, "bob_train_labels.pth"))
-    torch.save(mnist_test_labels, os.path.join(dir, "bob_test_labels.pth"))
+    torch.save(mnist_train_norm, os.path.join(dir, party1 + "_train.pth"))
+    torch.save(mnist_test_norm, os.path.join(dir, party1 + "_test.pth"))
+    torch.save(mnist_train_labels, os.path.join(dir, party2 + "_train_labels.pth"))
+    torch.save(mnist_test_labels, os.path.join(dir, party2 + "_test_labels.pth"))
+
+
+def split_train_v_test(dir="/tmp", party1="alice", party2="bob"):
+    """Gives Party 1 training data and Party 2 the test data """
+    mnist_norm, mnist_labels = _get_norm_mnist(dir)
+    mnist_train_norm, mnist_test_norm = mnist_norm
+    mnist_train_labels, mnist_test_labels = mnist_labels
+
+    torch.save(mnist_train_norm, os.path.join(dir, party1 + "_train.pth"))
+    torch.save(mnist_test_norm, os.path.join(dir, party2 + "_test.pth"))
+    torch.save(mnist_train_labels, os.path.join(dir, party1 + "_train_labels.pth"))
+    torch.save(mnist_test_labels, os.path.join(dir, party2 + "_test_labels.pth"))
+
+
+def main():
+    parser = argparse.ArgumentParser("Split data for use in Tutorials")
+    parser.add_argument(
+        "--option",
+        type=str,
+        choices={"features", "data", "features_v_labels", "train_v_test"},
+    )
+    parser.add_argument("--ratio", type=float, default=0.72)
+    parser.add_argument("--name_party1", type=str, default="alice")
+    parser.add_argument("--name_party2", type=str, default="bob")
+    parser.add_argument("--dest", type=str, default="/tmp")
+    args = parser.parse_args()
+
+    if args.option == "features":
+        split_features(
+            split=args.ratio,
+            dir=args.dest,
+            party1=args.name_party1,
+            party2=args.name_party2,
+        )
+    elif args.option == "data":
+        split_observations(
+            split=args.ratio,
+            dir=args.dest,
+            party1=args.name_party1,
+            party2=args.name_party2,
+        )
+    elif args.option == "features_v_labels":
+        split_features_v_labels(
+            dir=args.dest, party1=args.name_party1, party2=args.name_party2
+        )
+    elif args.option == "train_v_test":
+        split_train_v_test(
+            dir=args.dest, party1=args.name_party1, party2=args.name_party2
+        )
+    else:
+        raise ValueError("Invalid split option")
+
+
+if __name__ == "__main__":
+    main()
