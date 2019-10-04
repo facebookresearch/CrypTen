720d819cb8358bf529359e1b70a5fb4e55553a40
Shubho Sengupta <ssengupta@fb.com>
2019-10-03 23:22:17 -0700

    Add missing copyright headers (#12)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/CrypTen/pull/12
    
    Missing copyright headers
    
    Reviewed By: vshobha
    
    Differential Revision: D17758937
    
    fbshipit-source-id: ce525c1ec564b227b2426236cff8c3d810fc762e

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
