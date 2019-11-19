Fix pooling gradients (#158)
Summary:
Pull Request resolved: https://github.com/fairinternal/CrypTen/pull/158

* Compute gradient using conv2d with an average filter as the kernel
* Add support for encrypted `grad_output`
* Add support for tuple arguments and uneven pooling kernel sizes
* Fix multiple channel gradient

Reviewed By: knottb

Differential Revision: D18578356

fbshipit-source-id: 77c9fff1bafbf13e672e2ae690e0e745c35c28d8

crypten/gradients.py
@@ -1008,37 +1008,56 @@ class AutogradAvgPool2D(AutogradFunction):
             stride = (stride, stride)
         if isinstance(padding, (int, float)):
             padding = (padding, padding)
+        if isinstance(kernel_size, (int, float)):
+            kernel_size = (kernel_size, kernel_size)
 
         # perform average pooling:
         output = input.avg_pool2d(kernel_size, padding=padding, stride=stride)
 
         # store information for backward pass:
         ctx.save_multiple_for_backward(
-            (input.size(), output, kernel_size, padding, stride)
+            (input.shape, output, kernel_size, padding, stride)
         )
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
-        # TODO: Fix this backward. This seems to be correct when `grad_output`
-        # is all ones or when inchannels == 1, but is incorrect otherwise.
-
-        # create average pooling kernel:
-        input_size, output, kernel_size, padding, stride = ctx.saved_tensors
+        """Computes the gradient with respect to the input"""
+        input_shape, output, kernel_size, padding, stride = ctx.saved_tensors
         assert stride[0] == stride[1], "stride must be same in all axes"
         assert padding[0] == padding[1], "padding must be same in all axes"
-        inchannels = input_size[1]
-        ones = torch.ones(inchannels, inchannels, kernel_size, kernel_size)
 
-        # compute gradient with respect to input:
-        output_padding = torch.nn.grad._grad_input_padding(
-            grad_output, input_size, stride, padding, (kernel_size, kernel_size)
+        in_channels = input_shape[1]
+        # compute as d conv2d / d input with kernel as average filter
+        kernel = torch.ones(in_channels, 1, kernel_size[0], kernel_size[1]) / (
+            kernel_size[0] * kernel_size[1]
         )
-        return grad_output.conv_transpose2d(
-            grad_output.new(ones.div_(float(kernel_size ** 2))),
+
+        grad_input_padding = torch.nn.grad._grad_input_padding(
+            grad_output, input_shape, stride, padding, kernel_size
+        )
+
+        # set groups=in_channels so input gradient is computed per channel
+        if isinstance(grad_output, crypten.CrypTensor):
+            return grad_output.conv_transpose2d(
+                kernel,
+                bias=None,
+                stride=stride,
+                padding=padding,
+                output_padding=grad_input_padding,
+                groups=in_channels,
+                dilation=1,
+            )
+
+        return torch.conv_transpose2d(
+            grad_output,
+            kernel,
+            bias=None,
             stride=stride,
             padding=padding,
-            output_padding=output_padding,
+            output_padding=grad_input_padding,
+            groups=in_channels,
+            dilation=1,
         )
 
 

test/test_gradients.py
@@ -16,7 +16,6 @@ import torch
 import torch.nn.functional as F
 from crypten.autograd_cryptensor import AutogradCrypTensor
 from crypten.common.tensor_types import is_float_tensor
-from crypten.mpc import MPCTensor
 
 
 # Sizes for tensor operations
@@ -304,16 +303,11 @@ class TestGradients(MultiProcessTestCase):
         """Test pooling functions on encrypted tensor"""
         image_sizes = [(5, 5), (16, 7)]
         nchannels = [1, 3, 5]
-        nbatches = [1, 3, 5]
+        nbatches = [1, 5]
 
-        kernel_sizes = [1, 2]
-        paddings = [0, 1]
-        strides = [1, 2]
-
-        # TODO: Fix the following cases:
-        #
-        # 1) kernel_sizes / paddings / strides with tuples of uneven size (e.g. (1, 2))
-        # 2) Correct avg_pool2d backward for cases listed in its backward TODO
+        kernel_sizes = [1, 2, (2, 3)]
+        paddings = [1, (0, 0)]
+        strides = [1, (2, 2)]
 
         funcs = ["avg_pool2d", "max_pool2d"]
         for image_size, channels, batches, kernel_size in itertools.product(
@@ -323,10 +317,6 @@ class TestGradients(MultiProcessTestCase):
             image = get_random_test_tensor(size=size, is_float=True)
 
             for padding, stride, func in itertools.product(paddings, strides, funcs):
-                # TODO: Correct avg_pool2d backward:
-                if size[1] > 1 and func == "avg_pool2d":
-                    continue
-
                 # Skip invalid padding sizes
                 if kernel_size == 1 and padding == 1:
                     continue