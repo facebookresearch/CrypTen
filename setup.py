ef5a9b5aa395b3abd18703f89255a1d5521bad6d
Mark Ibrahim <marksibrahim@fb.com>
2020-02-05 11:23:13 -0800

    Package CrypTen on PyPI
    Summary:
    CrypTen is now a proper [PyPI package](https://pypi.org/project/crypten/0.1/), making installation one step: `pip install crypten`.
    
    * update readme with new installation instructions
    * add logo raw link so it renders on PyPI
    * update license and project url in `setup.py`
    * specific version of `torch` and `torchvision` now specified in `requirements.txt` and automatically installed via pip
    
    We can now also track installation statistics via PyPI.
    
    Differential Revision: D19746436
    
    fbshipit-source-id: 08cf96cac67d7cb9f2161af6777769170ed97dac

--- setup.py
+++ setup.py
@@ -13,20 +13,18 @@ import setuptools
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "crypten"))
 
-# Read description, license and requirements.
+# Read description and requirements.
 with open("README.md", encoding="utf8") as f:
     readme = f.read()
-with open("LICENSE") as f:
-    license = f.read()
 with open("requirements.txt") as f:
     reqs = f.read()
 
 # Set key package information.
 DISTNAME = "crypten"
-DESCRIPTION = "CrypTen: Private and secure machine learning in PyTorch."
+DESCRIPTION = "CrypTen: secure machine learning in PyTorch."
 LONG_DESCRIPTION = readme
 AUTHOR = "Facebook AI Research"
-LICENSE = license
+LICENSE = "MIT licensed, as found in the LICENSE file"
 REQUIREMENTS = (reqs.strip().split("\n"),)
 
 # Run installer.
@@ -39,6 +37,8 @@ if __name__ == "__main__":
         version="0.1",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
+        long_description_content_type="text/markdown",
+        url="https://github.com/facebookresearch/CrypTen",
         author=AUTHOR,
         license=LICENSE,
         setup_requires=["pytest-runner"],
