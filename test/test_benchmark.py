ba2a3c46db9909d6b84cb7ff303aba66930464d2
Mark Ibrahim <marksibrahim@fb.com>
2020-03-11 11:51:13 -0700

    Benchmarks dashboard (#69)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/CrypTen/pull/69
    
    An interactive dashboard to inspect CrypTen model and function benchmarks. Served at
    https://crypten.herokuapp.com/
    
    * Plotly dash app with monthly model / function benchmarks and a compare dates view
    * serving via Heroku using Procfile
    * Benchmarks generated with CPython (not Buck) running on Mac with world size 1
    * fix for comparison error measurement in `benchmark.py`
    * support for running `benchmark.py` from OSS or Buck
    * includes `run_historical_benchmarks.py` script to populate `dash_app/data`
    * includes unit tests for `benchmark.py`
    
    To run local version of dashboard from `dash_app` directory:
    * `pip install requirements.txt`
    * `python app.py`
    
    This starts a local version of the app in debugging mode.
    
    Differential Revision: D20160167
    
    fbshipit-source-id: 712d03a627630ebb7ef6652d59fb49ba20b1ddd4

---
+++ test/test_benchmark.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+
+try:
+    from ..benchmarks import benchmark
+except ValueError:
+    # ValueError is raised for relative import
+    # when calling $python -m unittest test/test_benchmark.py
+    from benchmarks import benchmark
+
+
+class TestBenchmark(unittest.TestCase):
+    def test_func_benchmarks_run(self):
+        """Ensure function benchmarks run without an exception"""
+        func_benchmarks = benchmark.FuncBenchmarks()
+        func_benchmarks.run()
+
+    def test_model_benchmarks_run(self):
+        """Ensure model benchmarks run without an exception"""
+        model_benchmarks = benchmark.ModelBenchmarks(n_samples=100, epochs=2)
+        model_benchmarks.run()
+
+    def test_func_benchmarks_data(self):
+        """Sanity check length and columns of function benchmarks"""
+        func_benchmarks = benchmark.FuncBenchmarks()
+        func_benchmarks.run()
+        expected_n_rows = len(benchmark.FuncBenchmarks.UNARY)
+        expected_n_rows += len(benchmark.FuncBenchmarks.BINARY)
+        expected_n_rows += len(benchmark.FuncBenchmarks.LAYERS)
+        n_rows = func_benchmarks.df.shape[0]
+        self.assertEqual(
+            n_rows,
+            expected_n_rows,
+            msg=f"function benchmarks {n_rows} rows. Expected {expected_n_rows}",
+        )
+        self.assertGreater(
+            func_benchmarks.df["total abs error"].sum(),
+            0,
+            msg="total abs error should be greater than 0",
+        )
+        self.assertTrue(
+            all(func_benchmarks.df["runtime"] > 0),
+            msg="runtime is less than or equal to zero",
+        )
+        self.assertTrue(
+            all(func_benchmarks.df["runtime crypten"] > 0),
+            msg="crypten runtime is less than or equal to zero",
+        )
+
+    def test_model_benchmarks_data(self):
+        """Sanity check length and columns of model benchmarks"""
+        model_benchmarks = benchmark.ModelBenchmarks(n_samples=100, epochs=2)
+        model_benchmarks.run()
+        expected_n_rows = 2 * len(model_benchmarks.MODELS)
+        n_rows = model_benchmarks.df.shape[0]
+        self.assertEqual(
+            n_rows,
+            expected_n_rows,
+            msg=f"model benchmarks have {n_rows} rows. Expected {expected_n_rows}",
+        )
+        self.assertTrue(
+            all(model_benchmarks.df["seconds per epoch"] > 0),
+            msg="seconds per epoch should be greater than 0",
+        )
+        self.assertTrue(
+            all(model_benchmarks.df["accuracy"] > 0)
+            and all(model_benchmarks.df["accuracy"] < 1.0),
+            msg="accuracy should be between 0 and 1.0",
+        )
+
+
+# This code only runs when executing the file outside the test harness
+if __name__ == "__main__":
+    unittest.main()
