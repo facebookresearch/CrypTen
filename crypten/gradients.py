Fix batchnorm input gradient (#143)
Summary:
Pull Request resolved: https://github.com/facebookresearch/CrypTen/pull/143

The gradients w.r.t. the input are computed incorrectly when batchnorm is in training mode. Specifically, the `backward()` implementation ignores the fact that the mean and variance are a function of the input, and so they contribute terms to the gradient of the input themselves.

The reason that this went unnoticed is because the batchnorm unit tests were incorrect as well: [this code](https://www.internalfb.com/intern/diffusion/FBS/browsefile/master/fbcode/deeplearning/projects/crypten/test/test_gradients.py?lines=734) does not pass the `training` input argument the way it should.

This diff fixes both the gradient and the unit test.

Reviewed By: marksibrahim

Differential Revision: D22275315

fbshipit-source-id: cb1cb6b19a063951ec1a95f8374171c4d80c8031

crypten/gradients.py
@@ -1479,20 +1479,18 @@ class AutogradBatchNorm(AutogradFunction):
         Returns: (weight * normalized input + bias) of shape `(N, C, +)`.
         """
 
-        # determine dimensions over which means and variances are computed
-        # for an input of shape (N, C, +), stats are computed over the C dimension
+        # determine dimensions over which means and variances are computed:
         stats_dimensions = list(range(x.dim()))
-        # remove C dimension
         stats_dimensions.pop(1)
-        # shape for broadcasting stats with x
+
+        # shape for broadcasting statistics with input:
         broadcast_shape = [1] * x.dim()
         broadcast_shape[1] = x.shape[1]
 
+        # compute mean and variance, track batch statistics:
         if training:
-            # track batch statistics
             mean = x.mean(stats_dimensions)
             variance = x.var(stats_dimensions)
-
             if running_mean is not None and running_var is not None:
                 running_var.set(running_var * (1.0 - momentum) + variance * momentum)
                 running_mean.set(running_mean * (1.0 - momentum) + mean * momentum)
@@ -1504,20 +1502,23 @@ class AutogradBatchNorm(AutogradFunction):
             mean = running_mean
             variance = running_var
 
+        # compute inverse variance:
         if torch.is_tensor(variance):
-            inv_var = 1 / torch.sqrt(variance + eps)
+            inv_var = 1.0 / torch.sqrt(variance + eps)
         else:
             inv_var = (variance + eps).pos_pow(-0.5)
 
-        # reshape shape (C) to broadcastable (1, C, 1, +)
+        # reshape shape (C) to broadcastable (1, C, 1, +):
         mean = mean.reshape(broadcast_shape)
         inv_var = inv_var.reshape(broadcast_shape)
         weight = weight.reshape(broadcast_shape)
         bias = bias.reshape(broadcast_shape)
 
+        # compute z-scores:
         x_norm = (x - mean) * inv_var
 
-        ctx.save_multiple_for_backward((x_norm, weight, inv_var))
+        # save context and return:
+        ctx.save_multiple_for_backward((x_norm, weight, inv_var, training))
         return x_norm * weight + bias
 
     @staticmethod
@@ -1541,21 +1542,48 @@ class AutogradBatchNorm(AutogradFunction):
                 with shape (C).
             bias_grad (cryptensor): gradient with respect to bias of shape (C).
         """
-        x_norm, weight, inv_var = ctx.saved_tensors
-        # determine dimensions over which means and variances are computed
-        # statistics are computed over C dimension for input shape: (N, C, +)
+
+        # retrieve context:
+        x_norm, weight, inv_var, training = ctx.saved_tensors
+
+        # determine dimensions over which means and variances are computed:
         stats_dimensions = list(range(len(grad_output.shape)))
         stats_dimensions.pop(1)
 
-        x_grad = grad_output * weight * inv_var
+        # shape for broadcasting statistics with output gradient:
+        broadcast_shape = [1] * grad_output.dim()
+        broadcast_shape[1] = grad_output.shape[1]
 
-        weight_grad = grad_output.mul(x_norm)
-        weight_grad = weight_grad.sum(stats_dimensions)
+        # compute gradient w.r.t. weight:
+        grad_weight = grad_output.mul(x_norm)
+        grad_weight = grad_weight.sum(stats_dimensions)
 
-        bias_grad = grad_output.clone()
-        bias_grad = bias_grad.sum(stats_dimensions)
+        # compute gradient w.r.t. bias:
+        grad_bias = grad_output.sum(stats_dimensions)
 
-        return (x_grad, weight_grad, bias_grad)
+        # compute gradient with respect to the input:
+        grad_output = grad_output.mul(weight)
+        grad_input = grad_output.mul(inv_var)
+        if training:
+
+            # compute gradient term that is due to the mean:
+            num_element = reduce(
+                lambda x, y: x * y, [grad_output.size(d) for d in stats_dimensions]
+            )
+            grad_mean = grad_output.sum(stats_dimensions)
+            grad_mean = grad_mean.reshape(broadcast_shape)
+            grad_mean = grad_mean.mul(inv_var.div(-num_element))
+
+            # compute gradient term that is due to the standard deviation:
+            grad_std = x_norm.mul(grad_output).sum(stats_dimensions)
+            grad_std = grad_std.reshape(broadcast_shape)
+            grad_std = x_norm.mul(grad_std).mul(inv_var.div(-num_element))
+
+            # put all the terms together:
+            grad_input = grad_input.add(grad_mean).add(grad_std)
+
+        # return gradients:
+        return (grad_input, grad_weight, grad_bias)
 
 
 @register_function("binary_cross_entropy")

test/test_gradients.py
@@ -702,38 +702,42 @@ class TestGradients:
         """
         Tests batchnorm forward and backward steps with training on / off.
         """
-        # sizes for 1D, 2D, and 3D batchnorm
-        # batch_size (dim=0) > 500 and increase tolerance to avoid flaky precision
-        # errors in inv_var, which involves sqrt and reciprocal
-        sizes = [(800, 5), (500, 8, 15), (600, 10, 3, 15)]
-        tolerance = 0.5
-
+        tolerance = 0.1
+        sizes = [(8, 5), (16, 3), (32, 5), (8, 6, 4), (8, 4, 3, 5)]
         for size in sizes:
-            for is_trainning in (False, True):
-                tensor = get_random_test_tensor(size=size, is_float=True)
-                tensor.requires_grad = True
-                encrypted_input = crypten.cryptensor(tensor)
+            for is_training in (False, True):
 
+                # sample input data, weight, and bias:
+                tensor = get_random_test_tensor(size=size, is_float=True)
+                encrypted_input = crypten.cryptensor(tensor)
                 C = size[1]
                 weight = get_random_test_tensor(size=[C], max_value=1, is_float=True)
                 bias = get_random_test_tensor(size=[C], max_value=1, is_float=True)
                 weight.requires_grad = True
                 bias.requires_grad = True
 
-                # dimensions for mean and variance
+                # dimensions over which means and variances are computed:
                 stats_dimensions = list(range(tensor.dim()))
-                # perform on C dimension for tensor of shape (N, C, +)
                 stats_dimensions.pop(1)
 
+                # dummy running mean and variance:
                 running_mean = tensor.mean(stats_dimensions).detach()
                 running_var = tensor.var(stats_dimensions).detach()
-                enc_running_mean = encrypted_input.mean(stats_dimensions)
-                enc_running_var = encrypted_input.var(stats_dimensions)
+                enc_running_mean = crypten.cryptensor(running_mean)
+                enc_running_var = crypten.cryptensor(running_var)
 
+                # compute reference output:
+                tensor.requires_grad = True
                 reference = torch.nn.functional.batch_norm(
-                    tensor, running_mean, running_var, weight=weight, bias=bias
+                    tensor,
+                    running_mean,
+                    running_var,
+                    weight=weight,
+                    bias=bias,
+                    training=is_training,
                 )
 
+                # compute CrypTen output:
                 encrypted_input.requires_grad = True
                 ctx = AutogradContext()
                 batch_norm_fn = crypten.gradients.get_grad_fn("batchnorm")
@@ -742,7 +746,7 @@ class TestGradients:
                     encrypted_input,
                     weight,
                     bias,
-                    training=is_trainning,
+                    training=is_training,
                     running_mean=enc_running_mean,
                     running_var=enc_running_var,
                 )
@@ -751,12 +755,12 @@ class TestGradients:
                 self._check(
                     encrypted_out,
                     reference,
-                    "batchnorm forward failed with trainning "
-                    f"{is_trainning} on {tensor.dim()}-D",
+                    "batchnorm forward failed with training "
+                    f"{is_training} on {tensor.dim()}-D",
                     tolerance=tolerance,
                 )
 
-                # check backward (input, weight, and bias gradients)
+                # check backward (input, weight, and bias gradients):
                 reference.backward(reference)
                 encrypted_grad = batch_norm_fn.backward(ctx, encrypted_out)
                 TorchGrad = namedtuple("TorchGrad", ["name", "value"])
@@ -765,13 +769,12 @@ class TestGradients:
                     TorchGrad("weight gradient", weight.grad),
                     TorchGrad("bias gradient", bias.grad),
                 ]
-
                 for i, torch_gradient in enumerate(torch_gradients):
                     self._check(
                         encrypted_grad[i],
                         torch_gradient.value,
-                        f"batchnorm backward {torch_gradient.name} failed"
-                        f"with training {is_trainning} on {tensor.dim()}-D",
+                        f"batchnorm backward {torch_gradient.name} failed "
+                        f"with training {is_training} on {tensor.dim()}-D",
                         tolerance=tolerance,
                     )
 