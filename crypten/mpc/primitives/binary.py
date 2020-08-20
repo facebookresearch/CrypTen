4e26fe57393401db7a8dbf17e240aadc8930ff5a
Brian Knott <brianknott@fb.com>
2020-08-20 11:55:36 -0700

    Write MPC protocol for random sampling without Provider
    Summary:
    - Adds protocol for generating uniform random samples by generating random binary shares with the correct encoding and converting to arithmetic shares.
    - Removes provider functions for rand()
    - Adds new test file for testing distributions of random sampling methods. I plan to use this to add new distributions to crypten looking forward (e.g. normal, exponential, and laplacian distributions).
    
    Differential Revision: D22989555
    
    fbshipit-source-id: 83f7c5c1c34cb64dd0ee9b9980419e71b6835e36

--- crypten/mpc/primitives/binary.py
+++ crypten/mpc/primitives/binary.py
@@ -87,6 +87,15 @@ class BinarySharedTensor(object):
         tensor.share = current_share ^ next_share
         return tensor
 
+    @staticmethod
+    def rand(*size, bits=64, device=None):
+        """
+        Generate a uniform random samples with a given size.
+        """
+        tensor = BinarySharedTensor(src=SENTINEL)
+        tensor.share = generate_kbit_random_tensor(*size, bitlength=bits, device=device)
+        return tensor
+
     @property
     def device(self):
         """Return the `torch.device` of the underlying _tensor"""
