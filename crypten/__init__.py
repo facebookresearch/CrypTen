563992fd92269e4686823ed5082f69485c48818a
Brian Knott <brianknott@fb.com>
2021-04-13 15:15:50 -0700

    Update RNG functions
    Summary:
    - Modified random seed generation to use `os.urandom(8)`.
    - added a `torch.manual_seed()` call in `crypten.init()` to prevent `marshallcline.run_multiprocess(n)` from returning all zeros in `crypten.rand()` when `n` is even. This happened due to issue in `numpy.randint()` when forking.
    - Adds `with no_grad():` to `crypten.rand()` and `crypten.randn()` definitions as desired.
    - Added tests in `test_context.py` to ensure that all random seeds differ and coordinate where expected.
    
    Differential Revision: D27750072
    
    fbshipit-source-id: ec12b6f20d53181a326b1212588fe81a8f7f6a51

--- crypten/__init__.py
+++ crypten/__init__.py
@@ -10,6 +10,7 @@ __version__ = "0.1.0"
 import builtins
 import copy
 import logging
+import os
 import warnings
 
 import crypten.common  # noqa: F401
@@ -59,15 +60,17 @@ def init(party_name=None, device=None):
         comm.get().set_name(party_name)
 
     # Setup seeds for Random Number Generation
+    local_seed = int.from_bytes(os.urandom(8), "big")
+    torch.manual_seed(local_seed)
     if comm.get().get_rank() < comm.get().get_world_size():
-        _setup_przs(device=device)
+        _setup_prng(device=device)
         if crypten.mpc.ttp_required():
             crypten.mpc.provider.ttp_provider.TTPClient._init()
 
 
 def init_thread(rank, world_size):
     comm._init(use_threads=True, rank=rank, world_size=world_size)
-    _setup_przs()
+    _setup_prng()
 
 
 def uninit():
@@ -174,7 +177,7 @@ def is_encrypted_tensor(obj):
     return isinstance(obj, CrypTensor)
 
 
-def _setup_przs(device=None):
+def _setup_prng(device=None):
     """
     Generate shared random seeds to generate pseudo-random sharings of
     zero. The random seeds are shared such that each process shares
@@ -208,10 +211,9 @@ def _setup_przs(device=None):
     # are different in all the parties. We use numpy's random here since
     # setting its seed to None will produce different seeds even from
     # forked processes.
-    import numpy
 
-    numpy.random.seed(seed=None)
-    next_seed = torch.tensor(numpy.random.randint(-(2 ** 63), 2 ** 63 - 1, (1,)))
+    seed = int.from_bytes(os.urandom(8), "big") - 2 ** 63
+    next_seed = torch.tensor(seed)
     prev_seed = torch.tensor([0], dtype=torch.long)  # placeholder
 
     # Send random seed to next party, receive random seed from prev party
@@ -234,7 +236,8 @@ def _setup_przs(device=None):
     comm.get().g1.manual_seed(prev_seed.item())
 
     # Create global generator
-    global_seed = torch.tensor(numpy.random.randint(-(2 ** 63), 2 ** 63 - 1, (1,)))
+    global_seed = int.from_bytes(os.urandom(8), "big") - 2 ** 63
+    global_seed = torch.tensor(global_seed)
     global_seed = comm.get().broadcast(global_seed, 0)
     comm.get().global_generator = torch.Generator()
     comm.get().global_generator.manual_seed(global_seed.item())
@@ -333,6 +336,11 @@ def load(f, load_closure=torch.load, **kwargs):
         save function in `crypten.save`. Additional kwargs are passed on
         to the closure.
     """
+    if "src" in kwargs:
+        raise SyntaxError(
+            "crypten.load() should not be used with `src` argument. Use load_from_party() instead."
+        )
+
     # TODO: Add support for loading from correct device (kwarg: map_location=device)
     if load_closure == torch.load:
         obj = load_closure(f)
@@ -436,18 +444,20 @@ def rand(*sizes, cryptensor_type=None):
     """
     Returns a tensor with elements uniformly sampled in [0, 1).
     """
-    if cryptensor_type is None:
-        cryptensor_type = get_default_cryptensor_type()
-    return __CRYPTENSOR_TYPES__[cryptensor_type].rand(*sizes)
+    with no_grad():
+        if cryptensor_type is None:
+            cryptensor_type = get_default_cryptensor_type()
+        return __CRYPTENSOR_TYPES__[cryptensor_type].rand(*sizes)
 
 
 def randn(*sizes, cryptensor_type=None):
     """
     Returns a tensor with normally distributed elements.
     """
-    if cryptensor_type is None:
-        cryptensor_type = get_default_cryptensor_type()
-    return __CRYPTENSOR_TYPES__[cryptensor_type].randn(*sizes)
+    with no_grad():
+        if cryptensor_type is None:
+            cryptensor_type = get_default_cryptensor_type()
+        return __CRYPTENSOR_TYPES__[cryptensor_type].randn(*sizes)
 
 
 def bernoulli(tensor, cryptensor_type=None):
