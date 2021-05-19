afc73bd0b598f7f087cdb836a088c75696fe86a8
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2021-05-19 12:22:00 -0600

    Reduce number of lru_cache lookups by __SPK_circuit.
    Generate all three of the constants __MASKS, __OUT_MASKS, and
    __MULTIPLIERS from a new function __SPK_circuit_constants that caches
    these constants per-device using functools.lru_cache.

--- crypten/mpc/primitives/circuit.py
+++ crypten/mpc/primitives/circuit.py
@@ -10,15 +10,23 @@ import math
 
 import torch
 
+# Cache masks and constants to skip computation during each call
+__BITS = torch.iinfo(torch.long).bits
+__LOG_BITS = int(math.log2(torch.iinfo(torch.long).bits))
 
-# Cached SPK masks are:
-# [0] -> 010101010101....0101  =                       01 x 32
-# [1] -> 001000100010....0010  =                     0010 x 16
-# [2] -> 000010000000....0010  =                 00001000 x  8
-# [n] -> [2^n 0s, 1, (2^n -1) 0s] x (32 / (2^n))
+
 @functools.lru_cache(maxsize=None)
-def __MASKS(device):
-    return torch.tensor(
+def __SPK_circuit_constants(device):
+    """
+    Generate the __MASKS, __OUT_MASKS, and __MULTIPLIERS constants
+    used by __SPK_circuit.
+    """
+    # Cached SPK masks are:
+    # [0] -> 010101010101....0101  =                       01 x 32
+    # [1] -> 001000100010....0010  =                     0010 x 16
+    # [2] -> 000010000000....0010  =                 00001000 x  8
+    # [n] -> [2^n 0s, 1, (2^n -1) 0s] x (32 / (2^n))
+    __MASKS = torch.tensor(
         [
             6148914691236517205,
             2459565876494606882,
@@ -31,17 +39,10 @@ def __MASKS(device):
         device=device,
     )
 
-# Cache other masks and constants to skip computation during each call
-__BITS = torch.iinfo(torch.long).bits
-__LOG_BITS = int(math.log2(torch.iinfo(torch.long).bits))
+    __MULTIPLIERS = torch.tensor([(1 << (2 ** iter + 1)) - 2 for iter in range(__LOG_BITS)], device=device)
+    __OUT_MASKS = __MASKS * __MULTIPLIERS
 
-@functools.lru_cache(maxsize=None)
-def __MULTIPLIERS(device):
-    return torch.tensor([(1 << (2 ** iter + 1)) - 2 for iter in range(__LOG_BITS)], device=device)
-
-@functools.lru_cache(maxsize=None)
-def __OUT_MASKS(device):
-    return __MASKS(device) * __MULTIPLIERS(device)
+    return __MASKS, __OUT_MASKS, __MULTIPLIERS
 
 
 def __SPK_circuit(S, P):
@@ -62,18 +63,19 @@ def __SPK_circuit(S, P):
     # Vectorize private AND calls to reduce rounds:
     SP = BinarySharedTensor.stack([S, P])
 
+    __MASKS, __OUT_MASKS, __MULTIPLIERS = __SPK_circuit_constants(SP.device)
+
     # fmt: off
     # Tree reduction circuit
     for i in range(__LOG_BITS):
-        in_mask = __MASKS(SP.device)[i]             # Start of arrows
-        out_mask = __OUT_MASKS(SP[1].device)[i]     # End of arrows
-        not_out_mask = out_mask ^ -1                # Not (end of arrows)
-        multiplier = __MULTIPLIERS(SP.device)[i]
+        in_mask = __MASKS[i]                # Start of arrows
+        out_mask = __OUT_MASKS[i]           # End of arrows
+        not_out_mask = out_mask ^ -1        # Not (end of arrows)
 
         # Set up S0, S1, P0, and P1
         P0 = SP[1] & out_mask               # Mask P0 from P
         S1P1 = SP & in_mask                 # Mask S1P1 from SP
-        S1P1._tensor *= multiplier          # Fan out S1P1 along arrows
+        S1P1._tensor *= __MULTIPLIERS[i]    # Fan out S1P1 along arrows
 
         # Update S and P
         update = P0 & S1P1                  # S0 ^= P0 & S1, P0 = P0 & P1
